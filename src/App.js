dd1515905c7c728f227a938d75f29a73fb9a62eb
maurawinstanley <maura.winstanley@colorado.edu>
2020-04-11 14:52:52 -0600

    map w / circles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Root from './root.js';
-function App() {
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Root from './root.js';
+// function App() {
 
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Hi </h1>
-      </header>
-        <Root />
-    </div>
-  );
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1> Hi </h1>
+//       </header>
+//         <Root />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import * as React from 'react';
+import {Component} from 'react';
+import {render} from 'react-dom';
+import MapGL, {Source, Layer} from 'react-map-gl';
+
+import ControlPanel from './control-panel';
+import {clusterLayer, clusterCountLayer, unclusteredPointLayer} from './layers';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiYWxvczEzOTEiLCJhIjoiY2s4cWpnZ24wMDRuYTNnbjB0dGMxbG5teSJ9.eixUyeHH4MIKgSn7jhK2_Q'; // Set your mapbox token here
+
+export default class App extends Component {
+  state = {
+    viewport: {
+      latitude: 40.67,
+      longitude: -103.59,
+      zoom: 3,
+      bearing: 0,
+      pitch: 0
+    }
+  };
+
+  _sourceRef = React.createRef();
+
+  _onViewportChange = viewport => this.setState({viewport});
+
+  _onClick = event => {
+    const feature = event.features[0];
+    const clusterId = feature.properties.cluster_id;
+
+    const mapboxSource = this._sourceRef.current.getSource();
+
+    mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+      if (err) {
+        return;
+      }
+
+      this._onViewportChange({
+        ...this.state.viewport,
+        longitude: feature.geometry.coordinates[0],
+        latitude: feature.geometry.coordinates[1],
+        zoom,
+        transitionDuration: 500
+      });
+    });
+  };
+
+  render() {
+    const {viewport} = this.state;
+
+    return (
+      <MapGL
+        {...viewport}
+        width="100%"
+        height="100%"
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        onViewportChange={this._onViewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        interactiveLayerIds={[clusterLayer.id]}
+        onClick={this._onClick}
+      >
+        <Source
+          type="geojson"
+          data="/stats.json"
+          cluster={true}
+          clusterMaxZoom={14}
+          clusterRadius={50}
+          ref={this._sourceRef}
+        >
+          <Layer {...clusterLayer} />
+          <Layer {...clusterCountLayer} />
+          <Layer {...unclusteredPointLayer} />
+        </Source>
+        <ControlPanel containerComponent={this.props.containerComponent} />
+      </MapGL>
+    );
+  }
 }
 
-export default App;
+export function renderToDom(container) {
+  render(<App />, container);
+}

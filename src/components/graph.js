c9f5c6ad938af4e622f16dbd989703d1894f83f8
maurawinstanley <maura.winstanley@colorado.edu>
2020-04-22 13:22:11 -0600

    unsecure endpt

--- src/components/graph.js
+++ src/components/graph.js
@@ -20,7 +20,8 @@ class Graph extends Component {
     super(props);
 
     this.state = {
-      url : "https://35.193.65.75:3001/",
+      // url : "https://35.193.65.75:3001/",
+      url: "http://localhost:3001/",
       selectedStat: "total_cases",
       dates: [],
       country: "World",

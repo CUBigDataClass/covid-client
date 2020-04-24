91d3300dc5afcf1853ade3a7e44db38c722df344
maurawinstanley <maura.winstanley@colorado.edu>
2020-04-23 20:35:17 -0600

    heroku issues

--- src/components/table.js
+++ src/components/table.js
@@ -73,11 +73,19 @@ componentDidMount() {
     // this.getStats("http://localhost:3001/data_nocoords?type=total_deaths", "total_deaths");
     // this.getStats("http://localhost:3001/data_nocoords?type=new_deaths", "new_deaths");
 
+    // this.getStats("http://74.125.72.101:3001/data_nocoords?type=new_cases", "new_cases");
+    // this.getStats("http://74.125.72.101:3001/data_nocoords?type=total_cases", "total_cases");
+    // this.getStats("http://74.125.72.101:3001/data_nocoords?type=total_deaths", "total_deaths");
+    // this.getStats("http://74.125.72.101:3001/data_nocoords?type=new_deaths", "new_deaths");
+
+
     this.getStats("http://35.193.65.75:3001/data_nocoords?type=new_cases", "new_cases");
     this.getStats("http://35.193.65.75:3001/data_nocoords?type=total_cases", "total_cases");
     this.getStats("http://35.193.65.75:3001/data_nocoords?type=total_deaths", "total_deaths");
     this.getStats("http://35.193.65.75:3001/data_nocoords?type=new_deaths", "new_deaths");
+
 }
+// 35.193.65.75
 
  render(){
 

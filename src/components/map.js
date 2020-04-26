2b06a16cc36445ce9c83eb64837fb9fff107a9e3
maurawinstanley <maura.winstanley@colorado.edu>
2020-04-26 17:21:46 -0600

    change to new vm ip

--- src/components/map.js
+++ src/components/map.js
@@ -9,7 +9,7 @@ class MapComp extends Component {
 
     this.state = {
 
-      url : "http://35.193.65.75:3001/",
+      url : "http://130.211.208.0:3001/",
       countries : [],
       coordinates: [],
       minLat: -6.1751,
